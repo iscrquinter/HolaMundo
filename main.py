456ef39b959aec5193b150a2758b138ed3658769
Ricardo Rafael Quintero Meza <iscrquinter@gmail.com>
2020-06-03 20:27:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# HellowWolrd.py - Programa Hola Mundo
+
+print("Hola Mundo")
+print("==========")
+
+nombre=input("Cual es tu nombre?")
+print("Hola ", nombre, "!")
+
+a=int(input("Dame un numero?"))
+b=int(input("Dame otro numero?"))
+
+c=a+b
+
+print("c="+str(c))
